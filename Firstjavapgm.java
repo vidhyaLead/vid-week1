20b3c21ccea8a92460812cd42e0b0e3e6a3d6691
vidhyaLead <91779058+vidhyaLead@users.noreply.github.com>
2021-10-02 23:54:35 +0530

    Add files via upload

---
+++ Firstjavapgm.java
@@ -0,0 +1,12 @@
+package simplepgm;
+
+public class Firstjavapgm {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+System.out.println("My first pgm");
+int Number = 10;
+}
+}
+
