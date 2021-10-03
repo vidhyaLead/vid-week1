5295b240a80ee0465e1843fe2a6fef152fc32260
vidhyaLead <91779058+vidhyaLead@users.noreply.github.com>
2021-10-03 10:07:15 +0530

    Add files via upload

---
+++ Evenoddnum.java
@@ -0,0 +1,19 @@
+package simplepgm;
+
+public class Evenoddnum {
+
+	public static void main(String[] args) {
+		int var=20;
+		
+		if(var % 2==0) {
+			System.out.println("the number is even");
+			
+		} else {
+			System.out.println("the number is not even");
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
